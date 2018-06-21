7d73e52dfe27bd21fad07ca5c6390769ae1020f3
Aaron Miller <millerac333@gmail.com>
2018-06-21 12:31:57 -0500

    finsihed chapter 5 practice

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-
+PART ONE
 Create an addExcitement function that should console.log() rows of words. It should take an array containing the words of a sentence and output them in the developer console.
 
 Example output:
@@ -26,3 +26,89 @@ The walrus danced through the trees in the light of
 The walrus danced through the trees in the light of the
 
 The walrus danced through the trees in the light of the moon ...
+
+PART TWO
+Adding Conditions to the Loop
+Then add logic to addExcitement that places an exclamation point (!) after every third word. This will require you to do some basic math in JavaScript, and use an if statement.
+
+Your job is to read the following English statement and write the equivalent in JavaSript code to make it work.
+
+If the current value of the counter variable can be evenly divided by 3 - using the JavaScript remainder operator - then add a single exclamation point (!) to the output.
+
+Example output:
+
+The
+
+The walrus
+
+The walrus danced!
+
+The walrus danced! through
+
+The walrus danced! through the
+
+The walrus danced! through the trees!
+
+The walrus danced! through the trees! in
+
+The walrus danced! through the trees! in the
+
+The walrus danced! through the trees! in the light!
+
+The walrus danced! through the trees! in the light! of
+
+The walrus danced! through the trees! in the light! of the
+
+The walrus danced! through the trees! in the light! of the moon! ...
+
+PART THREE
+Even More Excited
+Add logic in the addExcitement function to the increase the number of exclamation points after every third word. The number of exclamation points (!) will be determined by how many times the counter variable can be divided by 3.
+
+Example output:
+
+The
+
+The walrus
+
+The walrus danced!
+
+The walrus danced! through
+
+The walrus danced! through the
+
+The walrus danced! through the trees!!
+
+The walrus danced! through the trees!! in
+
+The walrus danced! through the trees!! in the
+
+The walrus danced! through the trees!! in the light!!!
+
+The walrus danced! through the trees!! in the light!!! of
+
+The walrus danced! through the trees!! in the light!!! of the
+
+The walrus danced! through the trees!! in the light!!! of the moon!!!! ...
+
+PART FOUR
+Function Arguments
+Add a new argument to the function so that a developer can specify which character should be displayed instead of it always being an exclamation point.
+
+// I want to use a question mark
+addExcitement(sentence, "?");
+Example output:
+
+The
+
+The walrus
+
+The walrus danced?
+
+The walrus danced? through
+
+The walrus danced? through the
+
+The walrus danced? through the trees??
+
+etc..
--- overly-excited.js
+++ overly-excited.js
@@ -1,29 +1,96 @@
-// Create an array that contains the words in the sentence
-let sentenceArray1 = ["The","walrus","danced","through","the","trees","in","the","light","of","the","moon"];
-let sentenceArray2 =["I","can","accept","failure,","but","I","can't","accept","not","trying"];
+// // PART ONE
+// // Create an array that contains the words in the sentence
+// let sentenceArray1 = ["The", "walrus", "danced", "through", "the", "trees", "in", "the", "light", "of", "the", "moon"];
+// let sentenceArray2 =["I", "can", "accept", "failure, ","but", "I", "can't", "accept", "not", "trying"];
 
+// /*
+//     The addExcitement function should be an impure function, and accept
+//     the array as the sole argument. It should iterate over the array
+//     and output the words to the browser console.
+// */
+// const addExcitment = function(theWordArray) {
 
+//     // Each time the for loop executes, you're going to add one more word to this string
+//     let buildMeUp = ""
 
-/*
-    The addExcitement function should be an impure function, and accept
-    the array as the sole argument. It should iterate over the array
-    and output the words to the browser console.
-*/
-const addExcitment = function(theWordArray) {
+//     for (let i = 0; i < theWordArray.length; i++) {
+//         // Concatenate the new word onto buildMeUp
+//         let newWord = theWordArray[i];
+//         buildMeUp += " " + newWord
+//         // Print buildMeUp to the console
+//         console.log(buildMeUp);
+//     }
 
-    // Each time the for loop executes, you're going to add one more word to this string
+// }
+
+// // Invoke the function and pass in the array
+// addExcitment(sentenceArray1)
+// addExcitment(sentenceArray2)
+
+//  // // PART TWO
+// let sentenceArray1 = ["The", "walrus", "danced", "through", "the", "trees", "in", "the", "light", "of", "the", "moon"];
+// let sentenceArray2 =["I", "can", "accept", "failure, ","but", "I", "can't", "accept", "not", "trying"];
+
+// const addExcitment = function(theWordArray) {
+//     let buildMeUp = ""
+//     let specialCharactrer = "!"
+    
+//     for (let i = 0; i < theWordArray.length; i++) {
+//             if ((i+1) % 3 === 0){
+//                 console.log(buildMeUp += `${theWordArray[i]} ${specialCharactrer}`);
+//             }
+//             else {
+//                 buildMeUp += " " + theWordArray[i];
+//             console.log(buildMeUp)
+//             }
+//         }
+//     }
+
+// addExcitment(sentenceArray1)
+// addExcitment(sentenceArray2)
+
+// // PART THREE
+// let sentenceArray1 = ["The", "walrus", "danced", "through", "the", "trees", "in", "the", "light", "of", "the", "moon"];
+// let sentenceArray2 =["I", "can", "accept", "failure, ","but", "I", "can't", "accept", "not", "trying"];
+
+// const addExcitment = function(theWordArray) {
+//     let buildMeUp = ""
+//     let specialCharactrer = "!" 
+    
+//     for (let i = 0; i < theWordArray.length; i++) {
+//             if ((i+1) % 3 === 0){
+//                 console.log(buildMeUp += `${theWordArray[i]} ${specialCharactrer}`);
+//                 specialCharactrer += "!"
+//             }
+//             else {
+//                 buildMeUp += " " + theWordArray[i];
+//             console.log(buildMeUp)
+//             }
+//         }
+//     }
+
+// addExcitment(sentenceArray1)
+// addExcitment(sentenceArray2)
+
+// // // PART FOUR
+let sentenceArray1 = ["The", "walrus", "danced", "through", "the", "trees", "in", "the", "light", "of", "the", "moon"];
+let sentenceArray2 =["I", "can", "accept", "failure, ","but", "I", "can't", "accept", "not", "trying"];
+
+const addExcitment = (theWordArray, specialCharacterInput) => {
     let buildMeUp = ""
-
+    let specialCharactrer = specialCharacterInput 
+    
     for (let i = 0; i < theWordArray.length; i++) {
-        // Concatenate the new word onto buildMeUp
-        let newWord = theWordArray[i];
-        buildMeUp += " " + newWord
-        // Print buildMeUp to the console
-        console.log(buildMeUp);
+            if ((i+1) % 3 === 0){
+                console.log(buildMeUp += `${theWordArray[i]} ${specialCharactrer}`);
+                specialCharactrer += specialCharacterInput;
+            }
+            else {
+                buildMeUp += " " + theWordArray[i];
+            console.log(buildMeUp);
+            }
+        }
     }
 
-}
-
-// Invoke the function and pass in the array
-addExcitment(sentenceArray1)
-addExcitment(sentenceArray2)
+addExcitment(sentenceArray1, "?")
+addExcitment(sentenceArray2, "$")
